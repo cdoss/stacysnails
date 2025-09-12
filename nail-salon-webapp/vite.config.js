84141b95f441cdc4b79c4c268c02e8270f56174f
Chandy Doss <chandy.doss@gmail.com>
2025-09-12 13:56:03 -0400

    updating repo name for gh pages

--- nail-salon-webapp/vite.config.js
+++ nail-salon-webapp/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'nail-salon-app', // replace with your actual repo name
+  base: 'stacysnails', // replace with your actual repo name
 })
